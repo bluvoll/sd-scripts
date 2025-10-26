7c0462219f612c14b29fbb05f0e879095d54211e
bluvoll <gordigo24@hotmail.com>
2025-10-26 02:13:56 -0500

    test

--- launch_all_train.sh
+++ launch_all_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-accelerate launch --gpu_ids 0,1 sdxl_train_network.py --dataset_config="dataset1.toml" --config_file="config1.toml" --flow_model  --flow_use_ot  --flow_timestep_distribution uniform --flow_uniform_static_ratio 2.5 --vae_custom_scale 0.1406 --vae_custom_shift -0.4743 --contrastive_flow_matching --cfm_lambda 0.5
+accelerate launch --gpu_ids 0,1 sdxl_train_network.py --dataset_config="dataset1.toml" --config_file="config1.toml" --flow_model  --flow_use_ot  --flow_timestep_distribution uniform --flow_uniform_static_ratio 2.5 --vae_custom_scale 0.1406 --vae_custom_shift -0.4743 --contrastive_flow_matching --cfm_lambda 0.05
 echo "All training jobs finished. Press any key to close..."
 read -n 1 -s -r -p ""
